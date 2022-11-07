6e6aa8e8115f0711e5f3dee22ca607e986777d2d
rtorres <rene.torres@csps-efpc.gc.ca>
2022-11-07 11:25:30 -0500

    update link

--- app.ts
+++ app.ts
@@ -1,6 +1,6 @@
 // import * as mod from "https://deno.land/std@0.154.0/fs/mod.ts";
 // import {readLines} from "https://deno.land/std/io/bufio.ts";
-import { copy } from "https://deno.land/std@0.154.0/fs/copy.ts";
+import { copy } from "https://deno.land/std@0.162.0/fs/copy.ts";
 
 
 let enTemplate = './src/bs-assets/templates/index_en.html';
